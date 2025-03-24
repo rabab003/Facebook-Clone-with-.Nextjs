a42bb8ba68536c9db4be5bf78e7c3e5f4474de8c
rabab003 <bintaliraq4@gmail.com>
2025-03-24 20:47:51 +0300

    8:47 pm

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -1,10 +1,22 @@
 import { useState } from "next-auth/client";
 
+import {
+  ChevronDownIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+} from "@heroicons/react/react/outline";
+
+import {
+  CalenderIcon,
+  ClockIcon,
+  DesktopComputerIcon,
+} from "@heroicons/react/react/solid";
+
 function Sidebar() {
   const [session, loading] = useState();
   return (
     <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
-      <Sidebar />
+      <SidebarRow Icon />
     </div>
   );
 }
